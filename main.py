6ebba88c7f9bc0b3545dd34498d9e5298006d877
Will Parker <parkerwill625@gmail.com>
2021-12-22 16:46:31 +0100

    Added sample workflow

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import StratifiedShuffleSplit, train_test_split
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.svm import LinearSVR, SVR
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.metrics import mean_squared_error
+
+from FeatureCreator import FeatureCreator
 
 
 def label_strata(dataset: pd.DataFrame) -> pd.DataFrame:
@@ -34,3 +42,37 @@ def create_train_test_sets(
 if __name__ == "__main__":
     dataset = pd.read_csv("housing.csv")
     X_train, X_test, y_train, y_test = create_train_test_sets(dataset)
+
+    num_features = [
+        "longitude",
+        "latitude",
+        "housing_median_age",
+        "total_rooms",
+        "total_bedrooms",
+        "population",
+        "households",
+        "median_income",
+    ]
+    num_pipeline = Pipeline(
+        [
+            ("scaler", StandardScaler()),
+            ("median_imputer", SimpleImputer(strategy="median")),
+            ("feature_creator", FeatureCreator()),
+        ]
+    )
+
+    cat_features = ["ocean_proximity"]
+    cat_pipeline = Pipeline([("onehot", OneHotEncoder())])
+
+    pipeline = ColumnTransformer(
+        [("num", num_pipeline, num_features), ("cat", cat_pipeline, cat_features)]
+    )
+
+    X_train_t = pipeline.fit_transform(X_train, y_train)
+
+    linear_svm = LinearSVR(random_state=42)
+    linear_svm.fit(X_train_t, y_train)
+
+    y_pred = linear_svm.predict(X_train_t)
+    rmse = mean_squared_error(y_train, y_pred, squared=False)
+    print(f"RMSE: ${rmse}")

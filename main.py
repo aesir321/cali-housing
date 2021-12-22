f2135a91750cdc624d3aca50a1b5b24e97ca36bd
Will Parker <parkerwill625@gmail.com>
2021-12-22 21:27:02 +0100

    Added random search for SVR model

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, RandomizedSearchCV
 from sklearn.pipeline import Pipeline
 from sklearn.svm import LinearSVR, SVR
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.preprocessing import StandardScaler
 from sklearn.impute import SimpleImputer
-from sklearn.compose import ColumnTransformer
-from sklearn.metrics import mean_squared_error
 import mlflow.sklearn
+from sklearn.datasets import fetch_california_housing
+from scipy.stats import reciprocal, uniform
 
 from FeatureCreator import FeatureCreator
 
@@ -35,26 +35,13 @@ def create_train_test_sets(
     X = dataset.drop("median_house_value", axis=1)
     y = dataset["median_house_value"]
     X_train, X_test, y_train, y_test = train_test_split(
-        X, y, stratify=strata_dataset[["house_val_cat", "income_cat"]]
+        X, y, stratify=strata_dataset[["house_val_cat", "income_cat"]], random_state=42
     )
     return X_train, X_test, y_train, y_test
 
 
-if __name__ == "__main__":
-    dataset = pd.read_csv("housing.csv")
-    X_train, X_test, y_train, y_test = create_train_test_sets(dataset)
-
-    num_features = [
-        "longitude",
-        "latitude",
-        "housing_median_age",
-        "total_rooms",
-        "total_bedrooms",
-        "population",
-        "households",
-        "median_income",
-    ]
-    num_pipeline = Pipeline(
+def create_pipeline(X: np.ndarray) -> np.ndarray:
+    pipeline = Pipeline(
         [
             ("scaler", StandardScaler()),
             ("median_imputer", SimpleImputer(strategy="median")),
@@ -62,16 +49,29 @@ if __name__ == "__main__":
         ]
     )
 
-    cat_features = ["ocean_proximity"]
-    cat_pipeline = Pipeline([("onehot", OneHotEncoder())])
+    X_train_t = pipeline.fit_transform(X_train)
 
-    pipeline = ColumnTransformer(
-        [("num", num_pipeline, num_features), ("cat", cat_pipeline, cat_features)]
-    )
+    return X_train_t
 
-    X_train_t = pipeline.fit_transform(X_train, y_train)
+
+if __name__ == "__main__":
+    housing = fetch_california_housing()
+    X = housing["data"]
+    y = housing["target"]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+    X_train_t = create_pipeline(X_train)
 
     mlflow.autolog()
-    linear_svm = LinearSVR(random_state=42)
+    param_distributions = {"gamma": reciprocal(0.001, 0.1), "C": uniform(1, 10)}
+    rnd_search_cv = RandomizedSearchCV(
+        SVR(),
+        param_distributions,
+        n_iter=10,
+        verbose=2,
+        cv=3,
+        random_state=42,
+        scoring="neg_root_mean_squared_error",
+        n_jobs=-1,
+    )
     with mlflow.start_run() as run:
-        linear_svm.fit(X_train_t, y_train)
+        rnd_search_cv.fit(X_train_t, y_train)
